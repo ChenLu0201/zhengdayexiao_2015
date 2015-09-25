5cdf594553e2f9f8930b27bff0a007c806df1a45
ChenLu0201 <blacksea.chen@hotmail.com>
2015-09-25 17:37:23 +0800

    [Chen Lu] fix jenkins issue

--- courses/8_CI_With_Jenkins/ci-sample-e2e/runTests.sh
+++ courses/8_CI_With_Jenkins/ci-sample-e2e/runTests.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 # Run E2E tests
 
-#install packages
-npm install
-
-#clear log
-rm -rf ss.log
-
-#start selenium server
-./node_modules/.bin/selenium-standalone start > ss.log &
-sleep 5s
-
 #run  jasmine tests
 ./node_modules/.bin/wdio wdio.conf.js
 
---
+++ courses/8_CI_With_Jenkins/ci-sample-e2e/startSeleniumServer.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Run E2E tests
+
+#install packages
+npm install
+
+#clear log
+rm -rf ss.log
+
+#start selenium server
+./node_modules/.bin/selenium-standalone start > ss.log &
+sleep 5s
