b991ee971b31497a268e827fd2d1b49fb27b93f4
ChenLu0201 <blacksea.chen@hotmail.com>
2015-09-25 17:07:52 +0800

    [Chen Lu] kill bootRun thread

--- courses/8_CI_With_Jenkins/ci-sample-e2e/runTests.sh
+++ courses/8_CI_With_Jenkins/ci-sample-e2e/runTests.sh
@@ -16,3 +16,6 @@ sleep 3s
 
 #stop selenium server
 ps -ef | grep node_modules/selenium-standalone | awk '{print $2}' | xargs kill -9
+
+#stop bootRun server
+ps -ef | grep 8_CI_With_Jenkins/ci-sample | awk '{print $2}' | xargs kill -9
